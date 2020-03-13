df60e90539c4c3f4c2887ca1bc97a3210df62e7d
Jai Flack <jaiflack@protonmail.com>
2020-03-13 16:41:40 +1000

    Remove need for debugmem.h in libmem-debug.h

--- libmem-debug.h
+++ libmem-debug.h
@@ -2,7 +2,18 @@
 
 #define LIBMEM_DEBUG_H
 
-#include "memdebug.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+void *malloc_mem_debug(size_t size, int line, const char *file, const char *func);
+void free_mem_debug(void *ptr, int line, const char *file, const char *func);
+void *calloc_mem_debug(size_t nmemb, size_t size, int line, const char *file, const char *func);
+void *realloc_mem_debug(void *ptr, size_t size, int line, const char *file, const char *func);
+
+#define print_mem_debug() fprint_mem_debug(stderr)
+int fprint_mem_debug(FILE *stream);
+int log_mem_debug(const char *fn, int8_t append);
 
 #define malloc(size) malloc_mem_debug(size, __LINE__, __FILE__, __func__)
 #define free(ptr) free_mem_debug(ptr, __LINE__, __FILE__, __func__)
